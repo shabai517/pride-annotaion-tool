824d96ba9222cc70185a66c3e21c4137bfd55441
shabai517 <shabai517@163.com>
2020-03-28 15:49:09 +0800

    remove step

--- src/components/step.vue
+++ src/components/step.vue
@@ -2,7 +2,7 @@
   <div class="step-wrapper">
       <Steps :current="step">
           <Step title="Search" content="Find your project to annotate"></Step>
-          <Step title="Check" content="Confirm your project information"></Step>
+          <!-- <Step title="Check" content="Confirm your project information"></Step> -->
           <Step title="Sample" content="Give the number of samples"></Step>
           <Step title="Annotate" content="Add the raw file"></Step>
       </Steps>
