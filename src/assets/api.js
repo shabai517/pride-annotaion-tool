a1ec4ce8752d4a8a3c9bf9f55a8a4630a4437df5
shabai517 <shabai517@163.com>
2020-05-19 15:19:12 +0800

    add modiifcation api request

--- src/assets/api.js
+++ src/assets/api.js
@@ -26,8 +26,34 @@ export default {
 					let resutls
 					let tempResutls = await	$http.get($store.state.baseApiURL+'/properties/findValuesByProperty',{params: query})
 					//TO DO: logic
-					console.log('tempResutls',tempResutls)
+					//console.log('tempResutls',tempResutls)
 					resutls = tempResutls
+					return resutls;
+				}
+				catch(e){
+					throw new Error(e.body.error);
+				}
+			},
+			getPTMsAttributes : async (query)=>{
+				try{
+					let resutls
+					let tempResutls = await	$http.get($store.state.baseApiURL+'/complexproperties/findPostTranslationalModifications',{params: query})
+					//TO DO: logic
+					let ntOptionsArray = []
+					//console.log('tempResutls',tempResutls)
+					if(tempResutls.body.length > 0){
+						for(let i in tempResutls.body){
+							let item = {
+								key:tempResutls.body[i].ontologyTerm.id,
+								label:tempResutls.body[i].ontologyTerm.name,
+								taOptionsArray:tempResutls.body[i].sites
+							}
+							ntOptionsArray.push(item)
+						}
+						resutls = ntOptionsArray
+					}
+					else
+						resutls = null
 					return resutls;
 				}
 				catch(e){
@@ -50,7 +76,7 @@ export default {
 				try{
 					let resutls
 					let tempResutls = await	$http.get($store.state.baseApiURL+ '/annotator/reagentValues',{params: query})
-					console.log('tempResutls',tempResutls);
+					//console.log('tempResutls',tempResutls);
 					//TO DO: logic
 					resutls = tempResutls
 					return resutls
@@ -63,7 +89,7 @@ export default {
 				try{
 					let resutls
 					let tempResutls = await	$http.get($store.state.baseApiURL + '/msruns/byProject',{params: query})
-					console.log('tempResutls',tempResutls);
+					//console.log('tempResutls',tempResutls);
 					//TO DO: logic
 					resutls = tempResutls
 					return resutls
