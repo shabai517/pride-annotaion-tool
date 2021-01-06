76231028b94823fe6199dd55628351cc0c50399c
shabai517 <shabai517@163.com>
2021-01-06 11:43:37 +0800

    decompress the gz file
    1. tell the upload file type:’tsv’ or ‘gz’

--- src/views/Srdf.vue
+++ src/views/Srdf.vue
@@ -38,6 +38,9 @@
 <script>
   var fs = require("fs");
   import selfTable from '@/components/table.vue'
+  import pako from 'pako'
+  var zlib = require('zlib');
+  const compressing = require('compressing');
   export default {
     name: 'srdf',
     data(){
@@ -65,99 +68,130 @@
         if (!files.length)
           return;
 
-        let reader = new FileReader();
-        reader.readAsText(files[0],'UTF-8');
-        this.loading=true
-        // this.$set
-        this.sampleCol=[]
-        this.sampleData=[]
-        this.keyList=[]
-        this.addedCol=[]
-        reader.onload =  async (e)=> {
-            try{
-                let arr = e.currentTarget.result.split('\n');
-                // this.sampleData = []
-                for(let i in arr){
-                    if(i == 0){ //for first row which is the col in the table
-                      let header = arr[i].split('\t');
-                      let properties = ''
-                      for(let j in header){
-                          // if(header[j].match(/\[/))
-                            properties+=(header[j]+', ')
-                      }
-                      let sdrf_properties = properties.slice(0,properties.length-2)
-                      let query = {
-                          sdrf_properties:sdrf_properties,
-                          // sdrf_properties:'Characteristics[organism], Characteristics[organism part], Characteristics[age], Characteristics[developmental stage], Characteristics[sex], Characteristics[disease], Characteristics[individual], comment[fraction identifier], comment[file uri], comment[instrument], comment[label], comment[cleavage agent details], comment[modification parameters], comment[modification parameters], comment[precursor mass tolerance], comment[fragment mass tolerance], comment[data file]',
-                        }
-                      let results = await this.$http.get(this.getTableDataAPI,{params: query})
-                      // this.sampleCol = []
-                      this.sampleCol.push({
-                          name: '#',
-                          key: 'index',
-                          // align: 'center',
-                          required: true,
-                      })
-                      //console.log('results',results)
-                      for(let i in results.body){
-                        let item = {
-                          name:results.body[i].freeTextColumn,
-                          key:results.body[i].freeTextColumn.replace(/\s+/g,"") + Math.floor(100000 + Math.random() * 900000),
-                          required:false,
-                          typeNode:results.body[i].templateColumn.typeNode ? results.body[i].templateColumn.typeNode : '',
-                          ontologyTerm:results.body[i].templateColumn.ontologyTerm ? results.body[i].templateColumn.ontologyTerm : null,
-                          searchable:results.body[i].templateColumn.searchable
-                        }
-                        this.keyList.push(item)
-                        this.sampleCol.push(item)
-                      }
+        console.log('files',files)
+        let fileType = files[0].name
+        let path = files[0].path
+        if(fileType.indexOf('.gz')>0){ //for gz file
+
+            console.log('进入gz')
+            compressing.gzip.uncompress(path, '/Users/jbai/EBI/test.json')
+              .then(() => {
+                  console.log('success');
+                  fs.readFile('/Users/jbai/EBI/test.json', 'utf-8', function (err, data) {
+                    if (err) {
+                      
+                    } else {
+                      var obj = JSON.parse(data)
+                      console.log('json',obj)
                     }
-                    else{ //for the table data
-                      if(!arr[i]){
-                        this.$Modal.warning({
-                              title: 'WARNING',
-                              content: '<p>The current file has some empty lines and the end, please remove it. Check specification</p>',
-                              onOk: () => {
-                                  
-                              },
-                          });
-                        continue
-                      }
-                      let body = arr[i].split('\t');
-                      let item = {}
-                      for(let j in body){
-                          item.index=parseInt(i)
-                          item[this.keyList[j].key]={
-                            value:body[j],
-                            checked:true,
-                            active:false,
-                            required:this.keyList[j].required
+                  });
+                  
+              })
+              .catch(err => {
+                console.error(err);
+              });
+            // var result = pako.inflate(e.target.result, { to: 'string' });
+            
+            // await compressing.gzip.compressFile
+        }
+        else{ //for normal tsv file
+          console.log('进入tsv')
+            let reader = new FileReader();
+            reader.readAsText(files[0],'UTF-8');
+            this.loading=true
+            // this.$set
+            this.sampleCol=[]
+            this.sampleData=[]
+            this.keyList=[]
+            this.addedCol=[]
+            reader.onload =  async (e)=> {
+                try{
+                    let arr = e.currentTarget.result.split('\n');
+                    // this.sampleData = []
+                    for(let i in arr){
+                        if(i == 0){ //for first row which is the col in the table
+                          let header = arr[i].split('\t');
+                          let properties = ''
+                          for(let j in header){
+                              // if(header[j].match(/\[/))
+                                properties+=(header[j]+', ')
                           }
-                          //console.log('item',item)
-                      }
-                      this.sampleData.push(item)
-                    }  
+                          let sdrf_properties = properties.slice(0,properties.length-2)
+                          let query = {
+                              sdrf_properties:sdrf_properties,
+                              // sdrf_properties:'Characteristics[organism], Characteristics[organism part], Characteristics[age], Characteristics[developmental stage], Characteristics[sex], Characteristics[disease], Characteristics[individual], comment[fraction identifier], comment[file uri], comment[instrument], comment[label], comment[cleavage agent details], comment[modification parameters], comment[modification parameters], comment[precursor mass tolerance], comment[fragment mass tolerance], comment[data file]',
+                            }
+                          let results = await this.$http.get(this.getTableDataAPI,{params: query})
+                          // this.sampleCol = []
+                          this.sampleCol.push({
+                              name: '#',
+                              key: 'index',
+                              // align: 'center',
+                              required: true,
+                          })
+                          //console.log('results',results)
+                          for(let i in results.body){
+                            let item = {
+                              name:results.body[i].freeTextColumn,
+                              key:results.body[i].freeTextColumn.replace(/\s+/g,"") + Math.floor(100000 + Math.random() * 900000),
+                              required:false,
+                              typeNode:results.body[i].templateColumn.typeNode ? results.body[i].templateColumn.typeNode : '',
+                              ontologyTerm:results.body[i].templateColumn.ontologyTerm ? results.body[i].templateColumn.ontologyTerm : null,
+                              searchable:results.body[i].templateColumn.searchable
+                            }
+                            this.keyList.push(item)
+                            this.sampleCol.push(item)
+                          }
+                        }
+                        else{ //for the table data
+                          if(!arr[i]){
+                            this.$Modal.warning({
+                                  title: 'WARNING',
+                                  content: '<p>The current file has some empty lines and the end, please remove it. Check specification</p>',
+                                  onOk: () => {
+                                      
+                                  },
+                              });
+                            continue
+                          }
+                          let body = arr[i].split('\t');
+                          let item = {}
+                          for(let j in body){
+                              item.index=parseInt(i)
+                              item[this.keyList[j].key]={
+                                value:body[j],
+                                checked:true,
+                                active:false,
+                                required:this.keyList[j].required
+                              }
+                              //console.log('item',item)
+                          }
+                          this.sampleData.push(item)
+                        }  
 
-                }
-                let tempAddedCol = await this.$http.get(this.getAddedColAPI)
-                for(let i in tempAddedCol.body){
-                    tempAddedCol.body[i].key = tempAddedCol.body[i].name.replace(/\s+/g,"") + Math.floor(100000 + Math.random() * 900000),
-                    tempAddedCol.body[i].required = false
-                }
-                this.addedCol = tempAddedCol.body
-                // console.log('addedCol',this.addedCol)
-                this.loading=false
+                    }
+                    let tempAddedCol = await this.$http.get(this.getAddedColAPI)
+                    for(let i in tempAddedCol.body){
+                        tempAddedCol.body[i].key = tempAddedCol.body[i].name.replace(/\s+/g,"") + Math.floor(100000 + Math.random() * 900000),
+                        tempAddedCol.body[i].required = false
+                    }
+                    this.addedCol = tempAddedCol.body
+                    // console.log('addedCol',this.addedCol)
+                    this.loading=false
 
-                //reset value for loading the same file
-                event.target.value = ''
-                //console.log(this.sampleCol,this.sampleData)
-                //this.$forceUpdate()
-            }
-            catch(e){
-              this.$Message.error({content:'File Read Error', duration:2});
-              console.log(e)
-            }
-        };
+                    //reset value for loading the same file
+                    event.target.value = ''
+                    //console.log(this.sampleCol,this.sampleData)
+                    //this.$forceUpdate()
+                }
+                catch(e){
+                  this.$Message.error({content:'File Read Error', duration:2});
+                  console.log(e)
+                }
+            };
+        }
+
+        
       },
       setClassName(name){
         if(name.match('sourcename') || name.match('characteristics'))
